8083ad761cc818f40fd2e7af50871e7a8a8b1a9a
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2019-01-25 14:34:31 +0530

    Update Face Recognition + Face Verification.py

--- Face Recognition + Face Verification.py
+++ Face Recognition + Face Verification.py
@@ -1,9 +1,4 @@
-#Let's load the required packages
-#First we will do Face Verification and then Face Recognition
-
-#Face Verification
-
-
+###Face Recognition
 from keras.models import Sequential
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
@@ -26,102 +21,6 @@ from inception_blocks_v2 import *
 
 #Loading the pretrained inception net model
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
-
-##Computing the Triplet Loss
-
-def triplet_loss(y_true, y_pred, alpha = 0.2):
-    """
-    Implementation of the triplet loss as defined by formula (3)
-    
-    Arguments:
-    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
-    y_pred -- python list containing three objects:
-            anchor -- the encodings for the anchor images, of shape (None, 128)
-            positive -- the encodings for the positive images, of shape (None, 128)
-            negative -- the encodings for the negative images, of shape (None, 128)
-    
-    Returns:
-    loss -- real number, value of the loss
-    """
-    
-    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
-   
-    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
-    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)), axis = -1)
-    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
-    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)), axis = -1)
-    # Step 3: subtract the two previous distances and add alpha.
-    basic_loss = pos_dist - neg_dist + alpha
-    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
-    loss = tf.reduce_sum(tf.maximum(basic_loss,0.0))
-  
-    return loss
-
-
-
-#Loading the model
-
-FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
-load_weights_from_FaceNet(FRmodel)
-
-#Creating the database of our Images
-
-database = {}
-database["danielle"] = img_to_encoding("images/danielle.png", FRmodel)
-database["younes"] = img_to_encoding("images/younes.jpg", FRmodel)
-database["tian"] = img_to_encoding("images/tian.jpg", FRmodel)
-database["andrew"] = img_to_encoding("images/andrew.jpg", FRmodel)
-database["kian"] = img_to_encoding("images/kian.jpg", FRmodel)
-database["dan"] = img_to_encoding("images/dan.jpg", FRmodel)
-database["sebastiano"] = img_to_encoding("images/sebastiano.jpg", FRmodel)
-database["bertrand"] = img_to_encoding("images/bertrand.jpg", FRmodel)
-database["kevin"] = img_to_encoding("images/kevin.jpg", FRmodel)
-database["felix"] = img_to_encoding("images/felix.jpg", FRmodel)
-database["benoit"] = img_to_encoding("images/benoit.jpg", FRmodel)
-database["arnaud"] = img_to_encoding("images/arnaud.jpg", FRmodel)
-
-
-#Verifying Images
-
-def verify(image_path, identity, database, model):
-    """
-    Function that verifies if the person on the "image_path" image is "identity".
-    
-    Arguments:
-    image_path -- path to an image
-    identity -- string, name of the person you'd like to verify the identity. Has to be a resident of the Happy house.
-    database -- python dictionary mapping names of allowed people's names (strings) to their encodings (vectors).
-    model -- your Inception model instance in Keras
-    
-    Returns:
-    dist -- distance between the image_path and the image of "identity" in the database.
-    door_open -- True, if the door should open. False otherwise.
-    """
-   
-   # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
-    encoding = img_to_encoding(image_path, model)
-    
-    # Step 2: Compute distance with identity's image (≈ 1 line)
-    dist = np.linalg.norm(database[identity]-encoding)
-    
-    # Step 3: Open the door if dist < 0.7, else don't open (≈ 3 lines)
-    if dist < 0.7:
-        print("It's " + str(identity) + ", welcome home!")
-        door_open = True
-    else:
-        print("It's not " + str(identity) + ", please go away")
-        door_open = False
-        
-   
-    return dist, door_open
-
-
-#Calling the above function
-verify("images/camera_0.jpg", "younes", database, FRmodel)
-
-
-
-###Now doing Face Recognition
 
 
 def who_is_it(image_path, database, model):
